acd7154e23c3cb4d3158483c117738591f51fe28
TIEMBOLOHO FANNY <fannytiem2@yahoo.fr>
2023-02-03 10:48:00 -0500

    commit

--- src/main/java/com/tfa/EmployeDepGatewayApplication.java
+++ src/main/java/com/tfa/EmployeDepGatewayApplication.java
@@ -2,10 +2,10 @@ package com.tfa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class EmployeDepGatewayApplication {
 
 	public static void main(String[] args) {
